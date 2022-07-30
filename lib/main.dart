630e95fd3d638192719dd91c36063cdbed5075c4
Atuoha <atuohainitiatives@gmail.com>
2022-07-30 09:20:56 +0100

    finished splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SplashScreen(),
       ),
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,41 +1,56 @@
 import 'package:flutter/material.dart';
 import '../constants/color.dart';
 import 'quiz_screen.dart';
+import 'package:flutter/services.dart';
 
 class SplashScreen extends StatelessWidget {
-  Widget customElevatedButton() {
-    return Container(
-      height:38,
-      width:150,
-      decoration: BoxDecoration(
-           color:primaryColor,
-           borderRadius: BorderRadius.circular(20),
+  void navigate() {}
+
+  Widget customElevatedButton(BuildContext context) {
+    return InkWell(
+      onTap: () => Navigator.of(context).pushNamed(MainScreen.routeName),
+      child: Container(
+        height: 38,
+        width: 150,
+        decoration: BoxDecoration(
+          color: primaryColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Center(
+          child: Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: const [
+              Text(
+                'Get Started',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(width: 10),
+              Icon(Icons.chevron_right, color: Colors.white)
+            ],
+          ),
+        ),
       ),
-      child:Wrap()
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+        ),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset('assets/images/question_bg2.png'),
           const SizedBox(height: 20),
-          customElevatedButton(),
-          ElevatedButton.icon(
-            style: ElevatedButton.styleFrom(
-              primary: primaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-            icon: const Icon(Icons.chevron_right),
-            onPressed: () =>
-                Navigator.of(context).pushNamed(MainScreen.routeName),
-            label: const Text('Get Started'),
-          )
+          customElevatedButton(context),
         ],
       ),
     );
